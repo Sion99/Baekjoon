222d5d5e9297aa59aabe6ac9ce246160f7fdf646
Sion99 <82511301+Sion99@users.noreply.github.com>
2021-06-28 22:12:33 +0900

    Create 11650.c

---
+++ 11650.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+typedef struct element {
+	int x;
+	int y;
+}element;
+element arr[100001];
+
+int compare(const void* a, const void* b) {
+	element num1 = *(element*)a;
+	element num2 = *(element*)b;
+
+	if (num1.x < num2.x)
+		return -1;
+	if (num1.x > num2.x)
+		return 1;
+	if (num1.y < num2.y)
+		return -1;
+	if (num1.y > num2.y)
+		return 1;
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &arr[i].x, &arr[i].y);
+	}
+	qsort(arr, n, sizeof(element), compare);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
+}
